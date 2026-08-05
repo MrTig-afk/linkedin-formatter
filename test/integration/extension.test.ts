d966da064630c43225e6324ab91f5112969e9b91
Kaushik <kaushiknaru2002@gmail.com>
2026-08-05 20:51:11 +1000

    test the editing path in a real extension host

--- test/integration/extension.test.ts
+++
@@ -1,9 +0,0 @@
-import * as assert from 'assert';
-import * as vscode from 'vscode';
-
-suite('Extension host smoke test', () => {
-  test('VS Code API is available', () => {
-    assert.ok(vscode.window);
-    assert.ok(vscode.workspace);
-  });
-});
